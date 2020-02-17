8702276c69a44cd915c18341d49a80817db78d76
patrickvonplaten <patrick.v.platen@gmail.com>
2020-02-17 20:08:39 +0100

    updated repetition penalty results with ctrl

--- debugging_scripts/run_all_generation.sh
+++ debugging_scripts/run_all_generation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a model_names=("distilgpt2" "gpt2" "xlm-clm-enfr-1024" "openai-gpt" "xlnet-base-cased")
+declare -a model_names=("ctrl" "distilgpt2" "gpt2" "xlm-clm-enfr-1024" "openai-gpt" "xlnet-base-cased")
 declare -a values=(1.1 1.2 1.3 1.4 1.5)
 
 for model_name in "${model_names[@]}"
